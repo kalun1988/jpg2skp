26ad56d9fb216225f358c4c76041fdbb97232ea3
sunlau <sun.lau@iheha.com>
2015-08-30 20:27:20 +0800

    added js upload image and use group to save cpu

--- jpg2skp.rb
+++ jpg2skp.rb
@@ -1,58 +1,44 @@
 require 'sketchup.rb'
+require 'app_safe_shutdown.rb'
 require 'csv'
 # Show the Ruby Console at startup so we can
 # see any programming errors we may make.
 SKETCHUP_CONSOLE.show
 
-# Add a menu item to launch our plugin.
-UI.menu("Plugins").add_item("JPG2SKP") {
-  # UI.messagebox("I'm about to draw stairs!") 
-  # Call our new method.
-  main
-
-}
 
 def main
-   # Create some variables.
-  run = 10
-  width = 10
-  thickness = 100
-  #csv
-
-  # Get handles to our model and the Entities collection it contains.
-  model = Sketchup.active_model
-  entities = model.entities
-
-	csv = CSV.read(File.path(File.dirname(__FILE__)+"/jpg2skp/jackie.txt"), headers:true)
+	# csv = CSV.read(File.path(File.dirname(__FILE__)+"/jpg2skp/jackie.txt"), headers:true)
+	csv = CSV.read(File.path("/Applications/AMPPS/www/lightcover/images/jackie.txt"), headers:true)
 	arr= csv.to_a
-	x_step=1
-	arr.each do |sub|
-		y_step=1
-	  sub.each do |int|
 
-	    # Calculate our stair corners.
-	    x1 = run * x_step
-	    x2 = run * (x_step + 1)
-	    y1 = run * y_step
-	    y2 = run * (y_step + 1)
-	    z= int.to_i
-	    # Create a series of "points", each a 3-item array containing x, y, and z.
-	    pt1 = [x1, y1, z]
-	    pt2 = [x2, y1, z]
-	    pt3 = [x2, y2, z]
-	    pt4 = [x1, y2, z]
-
-	    # Call methods on the Entities collection to draw stuff.
-	    new_face = entities.add_face pt1, pt2, pt3, pt4
-	    new_face.pushpull thickness
-	    y_step=y_step+1
-	  end
-	x_step=x_step+1
-	end
-
-
-
-
-
-
+	# Creates lots of boxes using components 
+	ent = Sketchup.active_model.entities 
+	s = 10
+	w = 10 
+	n = 127 
+	group = ent.add_group 
+	face = group.entities.add_face [0,0,0],[w,0,0],[w,w,0],[0,w,0] 
+	face.pushpull -w 
+	comp = group.to_component 
+	(0..n).each { |i| 
+		(0..n).each { |j| 
+				z=arr[i][j].to_i*30/255
+				transformation = Geom::Transformation.new([i*s,j*s,z]) 
+				ent.add_instance(comp.definition, transformation)
+		}
+	}
 end
+
+def model_save(sfile) 
+    sfile2 = sfile 
+    bret = Sketchup.active_model.save(sfile2) 
+    return(bret) if !bret 
+end 
+
+
+
+# UI.menu("Plugins").add_item("JPG2SKP") {
+  main()
+  model_save("test123")
+  # Sketchup::app_safe_shutdown()
+# }
